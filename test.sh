17308b44285702597fcfeddd20e0a94b55799e86
Robby <robby@k-state.edu>
2018-02-27 11:00:16 -0600

    Use shallow cloning in test script.

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ rm -fR runtime mill versions.properties out
 wget -q http://files.sireum.org/mill
 chmod +x mill
 wget -q https://raw.githubusercontent.com/sireum/v3/master/versions.properties
-git clone https://github.com/sireum/runtime runtime
+git clone --depth 1 https://github.com/sireum/runtime runtime
 TERM=xterm-color JAVA_OPTS="-Dorg.sireum.version.file=versions.properties" ./mill all slang.parser.shared.tests.test slang.tipe.shared.tests.test
