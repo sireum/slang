88a40add621b780edbf730c9b6e13388d23d1171
Robby <robby@k-state.edu>
2018-03-05 10:19:29 -0600

    Adapted to mill-build changes.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 rm -fR runtime mill versions.properties out
-wget -q http://files.sireum.org/mill
-chmod +x mill
+wget -q http://files.sireum.org/mill-standalone
+chmod +x mill-standalone
 wget -q https://raw.githubusercontent.com/sireum/v3/master/versions.properties
 git clone --depth 1 https://github.com/sireum/runtime runtime
-TERM=xterm-color JAVA_OPTS="-Dorg.sireum.version.file=versions.properties" ./mill all slang.parser.shared.tests.test slang.frontend.shared.tests.test
+JAVA_OPTS="-Dorg.sireum.version.file=versions.properties" ./mill-standalone all slang.parser.shared.tests.test slang.frontend.shared.tests.test
