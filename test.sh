15ba07e97b41cce00182202c491987ca4059c713
Robby <robby@k-state.edu>
2018-03-23 19:19:59 -0500

    Update test.sh.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-rm -fR runtime mill versions.properties out
-wget -q http://files.sireum.org/mill-standalone
+rm -fR runtime mill-standalone versions.properties out
+curl -Lo mill-standalone http://files.sireum.org/mill-standalone
 chmod +x mill-standalone
-wget -q https://raw.githubusercontent.com/sireum/v3/master/versions.properties
+curl -Lo versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
 git clone --depth 1 https://github.com/sireum/runtime runtime
-JAVA_OPTS="-Dorg.sireum.version.file=versions.properties" ./mill-standalone all slang.parser.shared.tests.test slang.frontend.shared.tests.test
+./mill-standalone all slang.parser.shared.tests.test slang.frontend.shared.tests.test
