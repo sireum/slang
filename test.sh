8aae05bb5bab3d39cda59f00110f8c8db580dc65
Robby <robby@k-state.edu>
2018-06-04 11:16:38 -0500

    Updated test script.

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
+export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 rm -fR runtime mill-standalone versions.properties out
 curl -Lo mill-standalone http://files.sireum.org/mill-standalone
 chmod +x mill-standalone
 curl -Lo versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
 git clone --depth 1 https://github.com/sireum/runtime runtime
-./mill-standalone all slang.parser.shared.tests.test slang.frontend.shared.tests.test
+$SCRIPT_HOME/mill-standalone all slang.parser.shared.tests.test slang.frontend.shared.tests.test
