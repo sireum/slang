48dfdbaf8c01f04a78f33fceba6d7c03331de4d4
Robby <robby@santoslab.org>
2021-04-02 16:08:22 -0500

    Updated project.cmd.

--- bin/project.cmd
+++ bin/project.cmd
@@ -1,28 +1,17 @@
-::#! 2> /dev/null                                             #
-@ 2>/dev/null # 2>nul & echo off & goto BOF                   #
-if [ -f "$0.com" ] && [ "$0.com" -nt "$0" ]; then             #
-  exec "$0.com" "$@"                                          #
-fi                                                            #
-rm -f "$0.com"                                                #
-if [ -z ${SIREUM_HOME} ]; then                                #
-  echo "Please set SIREUM_HOME env var"                       #
-  exit -1                                                     #
-fi                                                            #
-exec ${SIREUM_HOME}/bin/sireum slang run -n "$0" "$@"         #
+::#! 2> /dev/null                                   #
+@ 2>/dev/null # 2>nul & echo off & goto BOF         #
+if [ -z ${SIREUM_HOME} ]; then                      #
+  echo "Please set SIREUM_HOME env var"             #
+  exit -1                                           #
+fi                                                  #
+exec ${SIREUM_HOME}/bin/sireum slang run "$0" "$@"  #
 :BOF
 setlocal
 if not defined SIREUM_HOME (
   echo Please set SIREUM_HOME env var
   exit /B -1
 )
-set NEWER=False
-if exist %~dpnx0.com for /f %%i in ('powershell -noprofile -executionpolicy bypass -command "(Get-Item %~dpnx0.com).LastWriteTime -gt (Get-Item %~dpnx0).LastWriteTime"') do @set NEWER=%%i
-if "%NEWER%" == "True" goto native
-del "%~dpnx0.com" > nul 2>&1
 %SIREUM_HOME%\bin\sireum.bat slang run -n "%0" %*
-exit /B %errorlevel%
-:native
-%~dpnx0.com %*
 exit /B %errorlevel%
 ::!#
 // #Sireum
@@ -32,7 +21,6 @@ import org.sireum.project.ProjectUtil._
 import org.sireum.project.{JSON, Project}
 
 val library = "library"
-val test = "test"
 
 val slang = "slang"
 val ast = "ast"
@@ -52,7 +40,7 @@ val astShared = moduleShared(
 val parserShared = moduleShared(
   id = s"$slang-$parser",
   baseDir = homeDir / parser,
-  sharedDeps = ISZ(astShared.id, test),
+  sharedDeps = ISZ(astShared.id),
   sharedIvyDeps = ISZ("org.scalameta::scalameta::")
 )
 
